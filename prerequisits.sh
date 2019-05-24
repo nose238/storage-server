505199fd237005e2d69373357f47869ea300371f
Eduardo <nose238@hotmail.com>
2019-05-23 21:33:17 -0500

    first

--- prerequisits.sh
+++ prerequisits.sh
@@ -2,10 +2,12 @@
 # Installing C and C++ compiler
 yum install gcc gcc-c++ autoconf automake;
 # Downloading Jailkit
+yum install wget;
 wget https://olivier.sessink.nl/jailkit/jailkit-2.20.tar.gz;
 # Extracting files
 tar -vzxf jailkit-2.20.tar.gz;
 # Installing Jailkit
+cd jailkit-2.20;
 ./configure;
 make;
 make install;
